224f1c703c7623d719cfd9bf67cf96640f45b1c0
Ergo Nigola <ergo.nigola@gmail.com>
2018-03-16 14:01:12 +0200

    Avalehe, mängude ja kkk mallid lisatud.

--- application/views/pages/kkk.php
+++ application/views/pages/kkk.php
@@ -2,4 +2,22 @@
 	<div class="page-header">
 		<h1>KORDUMA KIPPUVAD KÜSIMUSED</h1>
 	</div>
+	<div>
+		<dl>
+			<dt>Küsimus 1</dt>
+			<dd>Vastus 1</dd>
+			<dt>Küsimus 2</dt>
+			<dd>Vastus 2</dd>
+			<dt>Lorem ipsum dolor sit amet, consectetur adipiscing
+			elit, sed do eiusmod tempor incididunt ut labore et dolore?</dt>
+			<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+			nisi ut aliquip ex ea commodo consequat.</dd>
+			<dt>Küsimus 4</dt>
+			<dd>Vastus 4</dd>
+			<dt>Küsimus 5</dt>
+			<dd>Vastus 5</dd>
+		</dl> 
+	</div>
 </div>
